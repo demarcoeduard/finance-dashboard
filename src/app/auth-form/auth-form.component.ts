431d2fa9fba6bb5b618c2cb3a1ada1c84b1aad65
De Marco Eduard <demarcoeduard@gmail.com>
2024-08-01 10:45:56 +0300

    added forms module functionality

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './auth-form.component.html',
   styleUrl: './auth-form.component.css'
 })
 export class AuthFormComponent {
 
+  onSubmit(form: NgForm) {
+    console.log(form)
+  }
 }
