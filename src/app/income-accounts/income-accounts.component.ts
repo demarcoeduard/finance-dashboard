0e6b35f9bc06f0d7c71a560adf5fc2d08d244db3
De Marco Eduard <demarcoeduard@gmail.com>
2024-08-07 20:39:27 +0300

    created income accounts designs

--- src/app/income-accounts/income-accounts.component.ts
+++ src/app/income-accounts/income-accounts.component.ts
@@ -1,12 +1,38 @@
+import { DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-income-accounts',
   standalone: true,
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './income-accounts.component.html',
   styleUrl: './income-accounts.component.css'
 })
 export class IncomeAccountsComponent {
-
+  accounts = [
+    {
+      name: 'account-name',
+      balance: 100000000
+    },
+    {
+      name: 'account-name',
+      balance: 100000000
+    },
+    {
+      name: 'account-name',
+      balance: 100000000
+    },
+    {
+      name: 'account-name',
+      balance: 100000000
+    },
+    {
+      name: 'account-name',
+      balance: 100000000
+    },
+    {
+      name: 'account-name',
+      balance: 100000000
+    },
+  ]
 }
