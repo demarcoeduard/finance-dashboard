6750a9c609c5fd72e9925cc52c3220fc537afcc5
De Marco Eduard <demarcoeduard@gmail.com>
2024-08-08 14:10:34 +0300

    created transfer & expense transactions sections

--- src/app/expense-transactions/expense-transactions.component.ts
+++ src/app/expense-transactions/expense-transactions.component.ts
@@ -1,18 +1,91 @@
+import { DecimalPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { FilterPipe } from '../filter.pipe';
 
 @Component({
   selector: 'app-expense-transactions',
   standalone: true,
-  imports: [],
+  imports: [DecimalPipe, FormsModule, FilterPipe],
   templateUrl: './expense-transactions.component.html',
   styleUrl: './expense-transactions.component.css'
 })
 export class ExpenseTransactionsComponent {
+  transactions = [
+    {
+      source: 'account-name1',
+      amount: 100000000,
+      receiver: 'account-name1.5'
+    },
+    {
+      source: 'account-name2',
+      amount: 100000000,
+      receiver: 'account-name2.5'
+    },
+    {
+      source: 'account-name3',
+      amount: 100000000,
+      receiver: 'account-name3.5'
+    },
+    {
+      source: 'account-name4',
+      amount: 100000000,
+      receiver: 'account-name4.5'
+    },
+    {
+      source: 'account-name5',
+      amount: 100000000,
+      receiver: 'account-name5.5'
+    },
+    {
+      source: 'account-name6',
+      amount: 100000000,
+      receiver: 'account-name6.5'
+    },
+  ];
   search = '';
   router = inject(Router);
+  alertType = '';
+  deleteAlert = false;
+  transactionIdx:number|null = NaN;
+  isOpen = false;
 
   constructor() {
     this.search = this.router.getCurrentNavigation()?.extras.state?.['data'];
+  }
+
+  onOpenAlert(idx: number|null) {
+    this.deleteAlert = !this.deleteAlert;
+    this.transactionIdx = idx;
+  }
+
+  onDelete() {
+    this.transactions.splice(this.transactionIdx!, 1);
+    this.onOpenAlert(NaN);
+  }
+
+  onCloseAlert() {
+    this.alertType = '';
+  }
+
+  onOpenForm() {
+    this.isOpen = !this.isOpen;
+  }
+
+  onSubmit(form: NgForm) {
+    let source = form.value.source;
+    let amount = form.value.amount;
+    let receiver = form.value.receiver;
+
+    this.transactions.push(
+      {
+        source: source,
+        amount: amount,
+        receiver: receiver
+      }
+    );
+
+    this.onOpenForm();
   }
 }
