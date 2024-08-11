2bc01689084ed16e5ce894f5a2fa8f7de529d434
De Marco Eduard <demarcoeduard@gmail.com>
2024-08-11 18:35:42 +0300

    added demo service to expense transactions

--- src/app/expense-transactions/expense-transactions.component.ts
+++ src/app/expense-transactions/expense-transactions.component.ts
@@ -1,8 +1,11 @@
 import { DecimalPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FilterPipe } from '../filter.pipe';
+import { DemoService } from '../services/demo.service';
+import { Data } from '../services/data.model';
+import { map, Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-expense-transactions',
@@ -11,57 +14,71 @@ import { FilterPipe } from '../filter.pipe';
   templateUrl: './expense-transactions.component.html',
   styleUrl: './expense-transactions.component.css'
 })
-export class ExpenseTransactionsComponent {
-  transactions = [
-    {
-      source: 'account-name1',
-      amount: 100000000,
-      receiver: 'account-name1.5'
-    },
-    {
-      source: 'account-name2',
-      amount: 100000000,
-      receiver: 'account-name2.5'
-    },
-    {
-      source: 'account-name3',
-      amount: 100000000,
-      receiver: 'account-name3.5'
-    },
-    {
-      source: 'account-name4',
-      amount: 100000000,
-      receiver: 'account-name4.5'
-    },
-    {
-      source: 'account-name5',
-      amount: 100000000,
-      receiver: 'account-name5.5'
-    },
-    {
-      source: 'account-name6',
-      amount: 100000000,
-      receiver: 'account-name6.5'
-    },
-  ];
+export class ExpenseTransactionsComponent implements OnInit {
   search = '';
   router = inject(Router);
   alertType = false;
   deleteAlert = false;
-  transactionIdx:number|null = NaN;
+  transactionIdx = NaN;
+  NaN = NaN;
   isOpen = false;
+  demoService = inject(DemoService);
+  expense!:Data['transactions']['expense'];
+  transactions!:Data['transactions']['expense'];
+  mainAccounts!:Data['accounts']['main'];
+  expenseAccounts!:Data['accounts']['expense'];
+  subscription!:Subscription;
+  destroyRef = inject(DestroyRef);
 
   constructor() {
     this.search = this.router.getCurrentNavigation()?.extras.state?.['data'];
   }
 
-  onOpenAlert(idx: number|null) {
+  ngOnInit(): void {
+    this.subscription = this.demoService.demo$.pipe(
+      tap(v => {
+        this.mainAccounts = JSON.parse(JSON.stringify(v.accounts.main));
+        this.expenseAccounts = JSON.parse(JSON.stringify(v.accounts.expense));
+      }),
+      map(v => v.transactions.expense)
+    ).subscribe(response => {
+      this.expense = response;
+
+      this.transactions = this.expense.map(v => ({
+        source: v.source,
+        amount: v.amount,
+        receiver: v.receiver
+      }))
+    })
+
+    this.destroyRef.onDestroy(() => this.subscription.unsubscribe());
+  }
+
+  onOpenAlert(idx: number) {
     this.deleteAlert = !this.deleteAlert;
     this.transactionIdx = idx;
   }
 
   onDelete() {
-    this.transactions.splice(this.transactionIdx!, 1);
+    let transaction = this.transactions[this.transactionIdx];
+    let source = transaction.source;
+    let amount = transaction.amount;
+    let receiver = transaction.receiver;
+
+    let sourceIdx = this.mainAccounts.findIndex(v => v.name === source);
+    let receiverIdx = this.expenseAccounts.findIndex(v => v.name === receiver);
+    let sourceAmount = this.mainAccounts[sourceIdx].balance + amount;
+    let receiverAmount = this.expenseAccounts[receiverIdx].balance - amount;
+
+    if (receiverAmount < 0) {
+      this.alertType = true;
+      return;
+    }
+
+    this.demoService.onEditBalance('main', sourceIdx, sourceAmount);
+    this.demoService.onEditBalance('expense', receiverIdx, receiverAmount);
+    this.demoService.onDeleteTransaction('expense', this.transactionIdx);
+
     this.onOpenAlert(NaN);
   }
 
@@ -78,13 +95,25 @@ export class ExpenseTransactionsComponent {
     let amount = form.value.amount;
     let receiver = form.value.receiver;
 
-    this.transactions.push(
-      {
-        source: source,
-        amount: amount,
-        receiver: receiver
-      }
-    );
+    let sourceIdx = this.mainAccounts.findIndex(v => v.name === source);
+    let receiverIdx = this.expenseAccounts.findIndex(v => v.name === receiver);
+    let sourceAmount = this.mainAccounts[sourceIdx].balance - amount;
+    let receiverAmount = this.expenseAccounts[receiverIdx].balance + amount;
+
+    if (sourceAmount < 0) {
+      this.alertType = true;
+      return;
+    }
+   
+    let data = {
+      source: source,
+      amount: amount,
+      receiver: receiver
+    }
+ 
+    this.demoService.onEditBalance('main', sourceIdx, sourceAmount);
+    this.demoService.onEditBalance('expense', receiverIdx, receiverAmount);
+    this.demoService.onCreateTransaction('expense', data);
 
     this.onOpenForm();
   }
