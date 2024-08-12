edc986e20ec40209cb9ee4460233f0c40f71bb9e
De Marco Eduard <demarcoeduard@gmail.com>
2024-08-12 09:33:51 +0300

    added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"finance-dashboard-46505","appId":"1:959168120206:web:9b388a3ef16198026b90df","databaseURL":"https://finance-dashboard-46505-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"finance-dashboard-46505.appspot.com","apiKey":"AIzaSyC5YfGpP6XexjqN5q_9GVlTsNTNX-HxL-Y","authDomain":"finance-dashboard-46505.firebaseapp.com","messagingSenderId":"959168120206","measurementId":"G-GX0K1LWQ77"})), provideAuth(() => getAuth()), provideDatabase(() => getDatabase())]
 };
