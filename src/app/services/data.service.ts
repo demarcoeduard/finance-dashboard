69afa1911ae91735180ae45b5bd0f461f7868815
De Marco Eduard <demarcoeduard@gmail.com>
2024-08-15 19:12:13 +0300

    created data service with necessary functions to handle demo and user data

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,88 @@
+import { inject, Injectable } from '@angular/core';
+import { DemoService } from './demo.service';
+import { DatabaseService } from './database.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private demoService = inject(DemoService);
+  private dbService = inject(DatabaseService);
+  demoMode = true;
+
+  constructor() {
+    let uid = localStorage.getItem('uid');
+    if (uid !== null) {
+      this.demoMode = false;
+    };
+  }
+
+  onSetMode() {
+    this.demoMode = !this.demoMode;
+  }
+
+  getData(): Observable<any> {
+    if (this.demoMode) {
+      return this.demoService.getData();
+    } else {
+      return this.dbService.getData();
+    };
+  }
+
+  createUserData(uid: string) {
+    return this.dbService.createUserData(uid);
+  }
+
+  fetchData(uid: string) {
+    this.dbService.fetchData(uid);
+  }
+
+  onCreateAccount(type: string, data: any) {
+    if (this.demoMode) {
+      this.demoService.onCreateAccount(type, data);
+    } else {
+      this.dbService.onCreateAccount(type, data);
+    };
+  }
+
+  onEditAccount(type: string, idx: number, data: any) {
+    if (this.demoMode) {
+      this.demoService.onEditAccount(type, idx, data);
+    } else {
+      this.dbService.onEditAccount(type, idx, data);
+    };
+  }
+
+  onDeleteAccount(type: string, idx: number) {
+    if (this.demoMode) {
+      this.demoService.onDeleteAccount(type, idx);
+    } else {
+      this.dbService.onDeleteAccount(type, idx);
+    };
+  }
+
+  onEditBalance(type: string, idx: number, amount: number) {
+    if (this.demoMode) {
+      this.demoService.onEditBalance(type, idx, amount);
+    } else {
+      this.dbService.onEditBalance(type, idx, amount);
+    };
+  }
+
+  onCreateTransaction(type: string, data: any) {
+    if (this.demoMode) {
+      this.demoService.onCreateTransaction(type, data);
+    } else {
+      this.dbService.onCreateTransaction(type, data);
+    };
+  }
+
+  onDeleteTransaction(type: string, idx: number) {
+    if (this.demoMode) {
+      this.demoService.onDeleteTransaction(type, idx);
+    } else {
+      this.dbService.onDeleteTransaction(type, idx);
+    }
+  }
+}
