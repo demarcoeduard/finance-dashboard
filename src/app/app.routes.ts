87720caced8ceb4d9e68cb812205a0abea407c28
De Marco Eduard <demarcoeduard@gmail.com>
2024-08-16 19:17:26 +0300

    added auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,11 @@ import { ExpenseAccountsComponent } from './expense-accounts/expense-accounts.co
 import { TransferTransactionsComponent } from './transfer-transactions/transfer-transactions.component';
 import { IncomeTransactionsComponent } from './income-transactions/income-transactions.component';
 import { ExpenseTransactionsComponent } from './expense-transactions/expense-transactions.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
-  { path: 'welcome', component: WelcomeComponent, data: {title: 'Welcome'} },
+  { path: 'welcome', component: WelcomeComponent, data: {title: 'Welcome'}, canActivate: [authGuard] },
   { path: 'auth-form', component: AuthFormComponent, data: {title: 'Authentication'} },
   { path: 'dashboard', component: DashboardComponent, data: {title: 'Dashboard'} },
   { path: 'main-accounts', component: MainAccountsComponent, data: {title: 'Main'} },
